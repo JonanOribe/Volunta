c4103723365dfe8f203038b40ff36677de8b795f
dfg884 <dfg884@gmail.com>
2018-11-25 23:28:55 +0100

    PHP
    Nueva carpeta PHP en la que irán los archivos de dicha extensión. Incluye:
    > database.php (datos de conexión con BD y funciones)
    > crearVoluntario.php (pasa info formulario homónimo a BD).
    
    Además:
    
    > Nuevo botón en vistaAdministrador para crear Voluntario.
    > Nuevo crearVoluntario.html (ventana emergente con formulario de creación).

---
+++ php/crearVoluntario.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once("database.php");
+
+$dni = $_POST["dni"];
+$nombre = $_POST["nombre"];
+$apellidos = $_POST["apellidos"];
+$telefono = $_POST["telefono"];
+$direccion = $_POST["direccion"];
+$ciudad = $_POST["ciudad"];
+$email = $_POST["email"];
+
+
+	
+		if(empty($dni) || empty($nombre) || empty($apellidos) || empty($telefono) || empty($direccion) || empty($ciudad) || empty($email)){
+			echo "Debes rellenar todos los campos";
+		}
+		else{
+			insertarPersona($con, $dni, $nombre, $apellidos, $telefono, $direccion, $ciudad, $email);
+			//header("Location: admin.php");
+			echo '<BUTTON onclick="window.close();">Close me.</BUTTON>';
+		}
+	
+	
+
+	cerrarConexion($con);
+
+?>
---
+++ php/database.php
@@ -0,0 +1,90 @@
+<?php
+	$server = "localhost";
+	$user = "root";
+	$pass = "";
+	$db = "volunta1";
+
+	$con = mysqli_connect($server, $user, $pass, $db) or die ("Error al conectar con la base de datos");
+	
+	function login($con, $dni){
+		$result = mysqli_query($con, "select * from persona where dni='".$dni."'");
+		if(mysqli_num_rows($result)==0){
+			return 0; //Si no existe el usuario devuelvo 0
+		}
+		else{
+			$usuario = mysqli_fetch_array($result);
+			return $usuario;//Si existe el usuario devuelvo un array con sus datos
+		}
+	}
+	
+	function cerrarConexion($con){
+		mysqli_close($con);
+	}
+	
+	////////////////////////////////////////////// FUNCIONES DE ADMINISTRADOR //////////////////////////////////////////////
+	
+		/*
+			FUNCIÓN PARA INSERTAR PERSONA
+			
+			ORDEN SQL --> INSERT INTO `persona`(`dni`, `nombre`, `apellidos`, `telefono`, `direccion`, `ciudad`, `email`) VALUES ([value-1],[value-2],[value-3],[value-4],[value-5],[value-6],[value-7])
+	*/
+	function insertarPersona($con, $dni, $nombre, $apellidos, $telefono, $direccion, $ciudad, $email){
+		mysqli_query($con, "insert into persona values('$dni', '$nombre', '$apellidos', '$telefono', '$direccion', '$ciudad', '$email')");
+	}
+	
+
+
+	function obtenerPersona($con, $dni){
+		$resultado = mysqli_query($con, "select * from persona where dni='$dni'");
+		if(mysqli_num_rows($resultado)==0){
+			return 0; //Si no existe el usuario devuelvo 0
+		}
+		else{
+			$persona = mysqli_fetch_array($resultado);
+			return $persona;//Si existe el usuario devuelvo un array con sus datos
+		}
+	}
+	
+	function modificarPersona($con, $dni, $apellido, $tipo_usuario){
+		mysqli_query($con, "update persona set apellido='$apellido', tipo_usuario=$tipo_usuario where dni='$dni'");
+	}
+	
+	function borrarPersona($con, $dni){
+		mysqli_query($con, "delete from persona where dni='$dni'");
+	}
+	
+	////////////////////////////////////////////// FUNCIONES DE EVENTOS //////////////////////////////////////////////
+	
+	function insertarEvento($con, $nombre){
+		mysqli_query($con, "insert into evento (nombre, latitud, longitud) values('$nombre', '$latitud', '$longitud')");
+	}
+	
+
+	function modificarEventoNombre($con, $id, $nombre){
+		mysqli_query($con, "update evento set nombre='$nombre' where id=$id");
+	}
+
+	function modificarEventoLatitud($con, $id, $latitud){
+		mysqli_query($con, "update evento set nombre='$latitud' where id=$id");
+	}
+
+	function modificarEventoLongitud($con, $id, $longitud){
+		mysqli_query($con, "update evento set nombre='$longitud' where id=$id");
+	}
+	
+	function borrarEvento($con, $id){
+		mysqli_query($con, "delete from evento where id=$id");
+	}
+	
+	////////////////////////////////////////////// PRUEBAS //////////////////////////////////////////////
+	
+	function listarVoluntarios($con){
+		$result = mysqli_query($con, "select * from persona where tipo_usuario=1");
+		$voluntarios = array();
+		while($fila = mysqli_fetch_array($result)){
+			$voluntarios[] = $fila;
+		}
+		return $voluntarios;//Devuelvo un array con los datos de todos los voluntarios
+	}
+		
+?>
