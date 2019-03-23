3f91ad9d42c716f4f76c3dbf5ed6a13c7774c8c5
Jon Ander Oribe <jonan.oribe@gmail.com>
2019-03-23 23:42:52 +0100

    Update vistaPrincipalVoluntario.js

--- src/vistaPrincipalVoluntario.js
+++ src/vistaPrincipalVoluntario.js
@@ -1,11 +1,3 @@
-//$(document).ready(function () {
-//
-//    $('#sidebarCollapse').on('click', function () {
-//        $('#sidebar').toggleClass('active');
-//    });
-//
-//});
-
 function recargarInfo() {
     document.getElementById('informacionEvento').style.display = 'block';
 }
@@ -21,8 +13,6 @@ function addRowHandlers() {
                     var datos = [];
                     var tituloEvento = row.getElementsByTagName("td")[1].innerHTML;
                     var organizador = row.getElementsByTagName("td")[3].innerHTML;
-                    //var tipoEvento = row.getElementsByTagName("td")[2].innerHTML;
-                    //var participantes = row.getElementsByTagName("td")[3].innerHTML;
                     var longitud = row.getElementsByTagName("td")[6].innerHTML;
                     var latitud = row.getElementsByTagName("td")[7].innerHTML;
 
@@ -45,8 +35,6 @@ function launchInfo(datos) {
     document.getElementById('informacionEvento').style.display = 'block';
     document.getElementById('tituloEvento').innerHTML = "Titulo evento: " + datos[0];
     document.getElementById('organizador').innerHTML = "Organizador: " + datos[1];
-    //document.getElementById('tipoEvento').innerHTML = "Tipo evento: " + datos[2];
-    //document.getElementById('participantes').innerHTML = "Participantes: " + datos[3];
     document.getElementById('longitud').innerHTML = "Tipo evento: " + datos[2];
     document.getElementById('latitud').innerHTML = "Participantes: " + datos[3];
 }
